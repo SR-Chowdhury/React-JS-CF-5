910bf4b581043b6fe3cd8e1a62c9ef81f58de977
Shihan Chowdhury <hackedgdown@gmail.com>
2022-10-30 16:42:14 +0000

    Practice problem didn't solved

--- src/practice-problem-1/index.jsx
+++ src/practice-problem-1/index.jsx
@@ -50,14 +50,16 @@ class PracticeProblem1 extends React.Component {
             <div className="container text-center">
                 <h1 className="my-3">Bismillah, Chapter Five: Practice Problem 1</h1>
                 <div className="mt-5">
-                    <button onClick={this.decrementTimer} className="btn btn-outline-secondary">-</button>
-                    <span className="countVar">{this.state.count}</span>
-                    <button onClick={this.incrementTimer} className="btn btn-outline-secondary">+</button>
+                    <button onClick={this.decrementTimer} className="btn btn-outline-warning">-</button>
+                    <span className="countVar">
+                        {this.state.count<10 ? "0"+this.state.count : this.state.count}
+                    </span>
+                    <button onClick={this.incrementTimer} className="btn btn-outline-success">+</button>
                 </div>
                 <div className="mt-3">
-                    <button onClick={this.startTimer} className="btn btn-outline-secondary">Start</button>
-                    <button onClick={this.pauseTimer} className="btn btn-outline-secondary">Pause</button>
-                    <button onClick={this.resetTimer} className="btn btn-outline-secondary">Reset</button>
+                    <button onClick={this.startTimer} className="btn btn-outline-primary">Start</button>
+                    <button onClick={this.pauseTimer} className="btn btn-outline-info">Pause</button>
+                    <button onClick={this.resetTimer} className="btn btn-outline-danger">Reset</button>
                 </div>
             </div>
 
